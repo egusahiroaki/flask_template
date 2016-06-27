fe06b029fd174c4641ad6674fee90d5b9fe5ad1c
egusahiroaki <egusa.hiroaki@gmail.com>
2016-06-27 22:38:33 +0900

    add templates

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "hello, world!"
+	return render_template('index.html', message="Hello")
+
+@app.route('/hello')
+def hello():
+	val = request.args.get("msg", "not defined")
+	return 'hellow word' + val
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+	return 'User %s' %  username
 
 if __name__ == "__main__":
 	app.run()
